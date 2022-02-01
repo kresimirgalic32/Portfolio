18b05579e5b71d8a9d891ee058a0c5973aabb20d
kresimirgalic32 <kresimir.galic32@gmail.com>
2022-02-01 14:49:06 +0100

    small progress

--- src/navbar/Curriculum.js
+++ src/navbar/Curriculum.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./Navbar";
 
 const Curriculum = () => {
+  const [isActive, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!isActive);
+  };
   return (
-    <div className="curriculum">
-      <div className="center">
-        <h1 className="text">Work in Progress</h1>
+    <div>
+      <div className={isActive ? "mobile " : "nav-close-mobile"}>
+        <button type="button" className="bttn" onClick={handleClick}></button>
+        <Navbar></Navbar>
+      </div>
+
+      <div className={isActive ? "dropdown-open" : null}>
+        <div className="curriculum">
+          <div className="nav-close">
+            <Navbar></Navbar>
+          </div>
+          <div className="dropdown">
+            <button
+              type="button"
+              className="btn-white"
+              onClick={handleClick}
+            ></button>
+          </div>
+          <div className="center">
+            <h1 className="text">Work in Progress</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/navbar/Projects.js
+++ src/navbar/Projects.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Navbar from "./Navbar";
 
 const Projects = () => {
+  const [isActive, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!isActive);
+  };
   return (
-    <div className="projects">
-      <div className="center">
-        <h1 className="text">Work in Progress</h1>
+    <div>
+      <div className={isActive ? "mobile " : "nav-close-mobile"}>
+        <button type="button" className="bttn" onClick={handleClick}></button>
+        <Navbar></Navbar>
+      </div>
+
+      <div className={isActive ? "dropdown-open" : null}>
+        <div className="projects">
+          <div className="nav-close">
+            <Navbar></Navbar>
+          </div>
+          <div className="dropdown">
+            <button
+              type="button"
+              className="btn"
+              onClick={handleClick}
+            ></button>
+          </div>
+
+          <div className="center">
+            <h1 className="text">Work in Progress</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
