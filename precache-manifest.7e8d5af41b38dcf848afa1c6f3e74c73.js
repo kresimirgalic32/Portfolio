7b7ce2ec1126b3a9b2b074dd89b64cfe0aa43cf7
kresimirgalic32 <91414126+kresimirgalic32@users.noreply.github.com>
2023-06-20 11:15:28 +0200

    Updates

---
+++ precache-manifest.7e8d5af41b38dcf848afa1c6f3e74c73.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d7a6508845e05516089d4acdb741a1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "22bc19e0ef6735d8bd87",
+    "url": "/Portfolio/static/css/main.03b25c23.chunk.css"
+  },
+  {
+    "revision": "8083fba6c8a8375b501a",
+    "url": "/Portfolio/static/js/2.1bfa10ac.chunk.js"
+  },
+  {
+    "revision": "22bc19e0ef6735d8bd87",
+    "url": "/Portfolio/static/js/main.607c916a.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "8aa8376dad1acca7bf7b1556f1412219",
+    "url": "/Portfolio/static/media/Chair-mobile.8aa8376d.jpg"
+  },
+  {
+    "revision": "00c505ce80a6bb4a678a97b2f128afeb",
+    "url": "/Portfolio/static/media/Chair.00c505ce.jpg"
+  },
+  {
+    "revision": "20ddb566b329e1add46ee6f104275576",
+    "url": "/Portfolio/static/media/Eye.20ddb566.jpg"
+  },
+  {
+    "revision": "9c7c9620628108322e606898443a8159",
+    "url": "/Portfolio/static/media/Orc.9c7c9620.jpg"
+  },
+  {
+    "revision": "2fde5c3d87e385815e871076c8b6ba34",
+    "url": "/Portfolio/static/media/Salon-Secrets-mobile.2fde5c3d.png"
+  },
+  {
+    "revision": "1ac827fe348aec28cf0e0ea7eacb30d1",
+    "url": "/Portfolio/static/media/Salon-Secrets.1ac827fe.png"
+  },
+  {
+    "revision": "b8b0d3fe7bcd602c5f04228954d1d0cf",
+    "url": "/Portfolio/static/media/back.b8b0d3fe.svg"
+  },
+  {
+    "revision": "cae2677c09fe04173fa902524ed3811c",
+    "url": "/Portfolio/static/media/dropdown-hover.cae2677c.svg"
+  },
+  {
+    "revision": "cfc5766466d893a3b0f6f697a9257fe3",
+    "url": "/Portfolio/static/media/dropdown-white.cfc57664.svg"
+  },
+  {
+    "revision": "11a61917fd35a9cfc444ddae6e1fac7c",
+    "url": "/Portfolio/static/media/dropdown.11a61917.svg"
+  },
+  {
+    "revision": "6aca5b76fce9fc65c53fd7d7cbe2b49a",
+    "url": "/Portfolio/static/media/head-mobile.6aca5b76.jpg"
+  },
+  {
+    "revision": "30b30b0719068be32b917a908199aa8c",
+    "url": "/Portfolio/static/media/kodius-assignment.30b30b07.png"
+  },
+  {
+    "revision": "404a5f519f9a2bef9ffe4852282c8a0d",
+    "url": "/Portfolio/static/media/orc-mobile.404a5f51.jpg"
+  },
+  {
+    "revision": "2e70d3a6aa4e85b50d7567db366852bf",
+    "url": "/Portfolio/static/media/raleway-v26-latin_cyrillic-regular.2e70d3a6.eot"
+  },
+  {
+    "revision": "a8131b49a59123f9679c5f8c4b6992b9",
+    "url": "/Portfolio/static/media/raleway-v26-latin_cyrillic-regular.a8131b49.woff2"
+  },
+  {
+    "revision": "b15b62119456595b6069c5e9e424d0da",
+    "url": "/Portfolio/static/media/raleway-v26-latin_cyrillic-regular.b15b6211.svg"
+  },
+  {
+    "revision": "bb799fe369d90ed2a218a648a3292f74",
+    "url": "/Portfolio/static/media/raleway-v26-latin_cyrillic-regular.bb799fe3.woff"
+  },
+  {
+    "revision": "dd220e98c461b9cc6f7834bce86f52f9",
+    "url": "/Portfolio/static/media/raleway-v26-latin_cyrillic-regular.dd220e98.ttf"
+  },
+  {
+    "revision": "1ec6a60e10467761377eacb4c7d04994",
+    "url": "/Portfolio/static/media/rock-climbing-hover.1ec6a60e.svg"
+  },
+  {
+    "revision": "e085d1b8cb815d9aa820806811ead8f0",
+    "url": "/Portfolio/static/media/rock-climbing.e085d1b8.svg"
+  },
+  {
+    "revision": "aa0903cc489f294a5c23066982a44f3d",
+    "url": "/Portfolio/static/media/timeline.aa0903cc.svg"
+  },
+  {
+    "revision": "bcfcb5a32ce7b3e9a7faf7fb3befaf8e",
+    "url": "/Portfolio/static/media/video.bcfcb5a3.mp4"
+  }
+]);
